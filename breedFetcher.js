1f680bf234f2caf4617d891797cf9d9305443a71
Chen Liang <c.liang@ualberta.ca>
2019-08-01 19:18:40 +0000

    testing finding breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
-const request = require('request');
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?name=${breedName}`;
+const request = require("request");
 
 
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?name=${breedName}`;
+  request(url, (error, resp, body) => {
+    if (error) {
+      callback("Error is: ", error, null);
+    }
+    if (body) {
+      const data = JSON.parse(body);
+      const breed = data[0];
+      if (breed) {
+        callback(null, breed.description);
+      } else {
+        callback(`Can't find: ${breedName}`, null)
+      }
+    }
+  });
+};
 
-request(url, (error, resp, body) => {
-
-  if (error) {
-    return console.log('Error is: ', error);
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Can\'t find: ${breedName}`);
-  }
-});
+module.exports = { fetchBreedDescription };
